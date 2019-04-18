71f41e3d4a89d794ccd0898d317a5f6ab52df1b0
Gerardo Keys <jotojry@icloud.com>
2019-04-18 15:24:28 -0700

    at home commit

--- home.js
+++ home.js
@@ -1,5 +1,15 @@
-/*Home*/
+/*
+    Home class will build the profile from user input. Will gather data
+    such as name, age, phone # and email. This information will be used
+    to populate the form for each of the options "Play", "Work", and
+    "School", depending on user choice. 
+
+
+*/
 class Home {
+
+    // Constructor will build the user object which will then be
+    // used later when user selects an option.
     constructor(name, age, phone, email) {
         this.name = name;
         this.age = age;
@@ -7,8 +17,20 @@ class Home {
         this.email = email;
     }
 
+    // Validates user input is populated.
     checkName() {
         let regex = /^$/;
         return regex.test(this.name);
     }
-}   
+
+
+
+}  
+
+class Listeners {
+
+
+
+
+
+}
