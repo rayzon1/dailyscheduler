790885e1fe96c0d04f0981a96e6fb9125f95d968
Gerardo Keys <jotojry@icloud.com>
2019-04-19 17:57:39 -0700

    at work commit

--- person.js
+++ person.js
@@ -18,25 +18,22 @@ class Person {
     }
 
     // Validates user input is populated.
-    
-    checkName() {
-        let regex = /^\D$/;
-        return regex.test(this.name);
-    }
     // Validates user age input is a valid phone number.
     checkAge() {
         let regex = /^\d+$/;
         return regex.test(this.age);
     }
 
+    
+
     getInformation() {
 
         $('#play').on("click", () => {
-            this.checkName() ? this.name = $('#name').val() : this.name = null;
-            this.checkAge() == false ? this.age = $('#age').val() : this.age = null;
-            $('#age').val("");
-            $('#name').val("");
-            this.phone = $('#phone').val();
+            this.name = $('#name').val();
+            //this.checkName() ? $('#name').val() = "" : this.name = null && $('#name').css("border", "1px solid red"); 
+            // this.checkAge() == false ? this.age = $('#age').val() : $('#age').css("border", "1px solid red");
+            // this.checkPhone() ? this.phone = $('#phone').val() : $('#phone').css("border", "1px solid red");;
+            
         
         })
     }
@@ -50,23 +47,59 @@ class Listeners extends Person {
         This will contain the various selected DOM elements as variables, as
         well as the listener functions associated with them.
     
-    
     */
 
-   replacePhone() {
+    constructor(name, age, phone, email) {
+        super(name, age, phone, email);
+    }
+
+
+    // listener for phone field which will replace number with formatted phone number.
+   checkPhone() {
         let regex = /^\(?(\d{3})\)?\s?-?(\d{3})\s?-?(\d{4})$/;
 
-        // listener for phone field which will replace number with formatted phone number.
         $('#phone').on("keyup keydown", function(){
-            if (regex.test(this.value)) {
+            if (regex.test(this.value) || this.value == "") {
                 const number = this.value.toString();
                 this.value = number.replace(regex, "($1) $2-$3");
                 $('#phone').css("border", "");
             } else {
                 $('#phone').css("border", "1px solid red");
             }
-        })
+        });
 
+    }
+
+    checkName() {
+        let regex = /^\D+$/;
+
+        $('#name').on("keyup keydown", function (){
+            if(regex.test(this.value)) {
+                $('#name').css("border", "");
+            } else {
+                $('#name').css("border", "1px solid red");
+            }
+            
+
+        });
+        
+    }
+
+    showSubmit() {
+
+
+        // use jquery ui .effect() function to shake input box if border-color red upon 
+        // submit.
+        $("#submit").on("click", function() {
+            event.preventDefault();
+            $.each($("input[type='text']"), function (){
+                if($(this).css('border-color') == 'rgb(255, 0, 0)'){
+                    $(this).effect("shake", {times:2}, 200);
+                }
+            })
+            
+        });
+       
     }
 
     
